5fdd30fa1a3ab10f42a8756ddbb408ab935c3037
mjnnhm225 <mjnnhmm225@gmail.com>
2025-09-03 17:47:10 +0300

    Create tiktok-icon.tsx

---
+++ src/components/layout/icons/tiktok-icon.tsx
@@ -0,0 +1,12 @@
+import { cn } from "@/lib/utils";
+
+export const TikTokIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className={cn("size-6", className)}
+  >
+    <path d="M16.6 5.82s.51.5 0 0A4.278 4.278 0 0 1 12.015 10.2a4.234 4.234 0 0 1-4.215 4.245V6.03h3.92v10.125a6.438 6.438 0 0 0 6.435-6.42c0-2.96-2.04-4.935-2.04-4.935Z"></path>
+  </svg>
+);
